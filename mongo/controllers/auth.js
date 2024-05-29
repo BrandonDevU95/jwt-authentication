25e675be70369cdaa44f2ef44311f11b4273ca9d
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-05-29 06:21:26 -0600

    feat: Implement user signup functionality with validation and error handling

--- mongo/controllers/auth.js
+++ mongo/controllers/auth.js
@@ -1,5 +1,45 @@
-function signup(req, res) {
-	res.send('signup').status(200);
+const User = require('../model/user');
+const { encryptPassword } = require('../utils/authPass');
+const { validateUser } = require('../schema/user');
+
+async function signup(req, res) {
+	const user = validateUser(req.body);
+
+	if (!user.success) {
+		return res.status(400).json({ error: JSON.parse(user.error.message) });
+	}
+
+	const hashedPassword = await encryptPassword(user.data.password);
+
+	if (hashedPassword.error) {
+		return res.status(500).json({ error: hashedPassword.message });
+	}
+
+	user.data.password = hashedPassword;
+
+	const newUser = new User(user.data);
+	try {
+		await newUser.save();
+		return res.status(201).json({ message: 'User created successfully' });
+	} catch (error) {
+		if (error.code === 11000) {
+			// Código de error de duplicación
+			const duplicateField = Object.keys(error.keyPattern)[0]; // Obtener el campo duplicado
+			let errorMessage = '';
+
+			if (duplicateField === 'username') {
+				errorMessage = 'Username already in use.';
+			} else if (duplicateField === 'email') {
+				errorMessage = 'Email already in use.';
+			} else {
+				errorMessage = 'Duplicate field error.';
+			}
+
+			res.status(400).json({ error: errorMessage });
+		} else {
+			res.status(500).json({ error: 'Internal Server Error' });
+		}
+	}
 }
 
 module.exports = {
