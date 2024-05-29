10ec87ef95c4b235348e7fe3fbdba498473c9966
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-05-29 13:31:19 -0600

    feat: Add cookie-parser middleware for handling cookies

--- mongodb/app.js
+++ mongodb/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
@@ -8,6 +9,9 @@ const authRoutes = require('./router/auth');
 
 //Configure Body Parser
 app.use(express.json());
+
+// Configurar cookie-parser
+app.use(cookieParser());
 
 //Configure CORS
 app.use(cors());
