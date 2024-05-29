d3c6eaa5e8d7996333b75fbfacd3c50e3820837b
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-05-29 17:48:06 -0600

    refactor: Update userAuth middleware to handle token expiration and refresh token

--- mongodb/middlewares/authenticateToken.js
+++ mongodb/middlewares/authenticateToken.js
@@ -1,15 +1,59 @@
 const jwt = require('../utils/jwt');
 
-function authenticateToken(req, res, next) {
-	const token = req.cookies['access_token'];
-	if (!token) return res.sendStatus(401);
+function userAuth(req, res, next) {
+	const accessToken = req.cookies['access_token'];
+	const refreshToken = req.cookies['refresh_token'];
 
-	const decoded = jwt.verifyToken(token);
+	if (!accessToken && !refreshToken)
+		return res
+			.status(401)
+			.json({ error: 'Unauthorized the token is required' });
 
-	if (!decoded) return res.status(403).json({ error: 'Forbidden' });
+	try {
+		const accessTokenPayload = jwt.decodedToken(accessToken);
+		const refreshTokenPayload = jwt.decodedToken(refreshToken);
 
-	req.user = decoded;
+		if (!accessTokenPayload || !refreshTokenPayload) {
+			res.clearCookie('access_token');
+			res.clearCookie('refresh_token');
+			return res
+				.status(401)
+				.json({ error: 'Unauthorized invalid token' });
+		}
+
+		const expAccessToken = accessTokenPayload.exp;
+		const expRefreshToken = refreshTokenPayload.exp;
+
+		const currentDate = new Date().getTime();
+
+		//Evaluamos si el access_token ha expirado, si ya ha expirado, se evalua si el refresh_token ha expirado
+		if (expAccessToken <= currentDate) {
+			if (expRefreshToken <= currentDate) {
+				//Si el refresh_token ha expirado, se elimina la cookie y se envía un error
+				res.clearCookie('access_token');
+				res.clearCookie('refresh_token');
+				return res.status(401).json({ error: 'Token has expired' });
+			} else {
+				//Si el refresh_token no ha expirado, se genera un nuevo access_token y se asigna a la cookie
+				const newAccessToken = jwt.generateToken(accessTokenPayload);
+				console.log('newAccessToken', newAccessToken);
+				res.cookie('access_token', newAccessToken, {
+					httpOnly: true,
+					secure: process.env.NODE_ENV === 'production',
+					sameSite: 'Strict',
+					maxAge: 5 * 60 * 1000,
+				});
+			}
+		}
+
+		//Si el access_token no ha expirado, se asigna el payload del access_token a req.user
+		req.user = accessTokenPayload;
+		next();
+	} catch (error) {
+		return res.status(403).json({ error: 'Forbidden' });
+	}
+
 	next();
 }
 
-module.exports = authenticateToken;
+module.exports = { userAuth };
